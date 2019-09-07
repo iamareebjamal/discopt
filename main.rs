965a0aac0b542aee0ef5c361241a7315b7562cd9
iamareebjamal <jamal.areeb@gmail.com>
2019-09-08 02:45:40 +0530

    chore: Add rust version

--- main.rs
+++ main.rs
@@ -1,5 +1,78 @@
+use std::env;
+use std::fs::File;
+use std::io::Read;
+use std::io;
+use std::cmp::max;
+
+#[derive(Debug)]
+struct Item {
+    index: usize,
+    value: usize,
+    weight: usize
+}
+
+fn parse(input_data: &str) -> Result<(usize, Vec<Item>), Box<std::error::Error>> {
+    let mut iter = input_data.lines();
+
+    let mut size_capacity: Vec<usize> = iter.next()
+        .unwrap()
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+    let size = size_capacity[0];
+    let capacity = size_capacity[1];
+
+    let mut items: Vec<Item> = iter.enumerate()
+        .map(|(index, line)| {
+            let item_lines: Vec<usize> = line.split_whitespace()
+                .map(|s| s.parse().unwrap())
+                .collect();
+            Item {
+                index,
+                value: item_lines[0],
+                weight: item_lines[1]
+            }
+        }).collect();
+
+    return Ok((capacity, items))
+}
+
+fn solve(capacity: usize, items: &[Item]) -> (usize, &[u32]) {
+    let mut cache: Vec<Vec<usize>> = vec![vec![0; capacity + 1]; items.len() + 1];
+
+    for i in 0..items.len() + 1 {
+        for w in 0..capacity + 1 {
+            if i == 0 || w == 0 {
+                cache[i][w] = 0;
+            } else if items[i - 1].weight <= w {
+                cache[i][w] = max(cache[i - 1][w], items[i - 1].value + cache[i - 1][w - items[i - 1].weight]);
+            } else {
+                cache[i][w] = cache[i - 1][w];
+            }
+        }
+    }
+
+    (cache[items.len()][capacity], &[])
+}
+
+fn run(file_name: &str) -> io::Result<()> {
+    let mut file = File::open(file_name)?;
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    let (capacity, items) = parse(&contents).unwrap();
+    let (value, taken) = solve(capacity, &items);
+
+    println!("{}", value);
+
+    Ok(())
+}
+
 fn main() {
-    let a = 2 + 3;
-    let b = a + 4;
-    println!("{}", b)
+    let args: Vec<String> = env::args().collect();
+
+    let file_name = &args[1];
+
+    run(file_name);
 }
--- solver_rust.py
+++ solver_rust.py
@@ -4,8 +4,9 @@
 import os
 from subprocess import Popen, PIPE
 
+
 def solve_it(file_location):
-    Popen(['rustc', '-O', 'main.rs']).communicate()
+    Popen('rustc -A warnings main.rs'.split(' ')).communicate()
     process = Popen(['./main', file_location], stdout=PIPE)
     (stdout, stderr) = process.communicate()
 
@@ -17,7 +18,7 @@ import sys
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         file_location = sys.argv[1].strip()
-        print solve_it(file_location)
+        print(solve_it(file_location).decode('utf-8'))
     else:
         print('This test requires an input file.  Please select one from the data directory. (i.e. python solver.py ./data/ks_4_0)')
 
